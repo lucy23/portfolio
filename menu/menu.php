36db9018a3c3c3f5ed01517be060a9487a2c5def
lucy <lucysigler23@gmail.com>
2016-01-24 13:46:58 +0100

    guide style

---
+++ menu/menu.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/menu.css">
+
+  <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+
+  <link href='https://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
+  
+  <link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+
+<title>Lucy Sigler</title>
+
+ </head>
+ <body>
+ 	<header>
+ 		<div class="menu">
+
+      <h1 class="titulo">Lucy Sigler</h1> 
+        <nav>
+          <ul>
+          
+	            <li><a href="formulario.html">Contacto</a></li>
+	            <li><a href="buttons.html">Buttons</a>
+	            <li><a href="color.html">Colores</a></li>
+	           	<li><a href="type.html">Tipografía</a></li>
+              
+          </ul>
+          
+           
+      </nav>
+    
+    </div>
+    </header>
+
+   
+    	<img src="img/wood.jpg">
+    	
+
+
+
+	</body>
+</html>
